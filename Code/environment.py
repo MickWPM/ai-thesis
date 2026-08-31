7b617f336fa426ec4789c76797eb67a0f2b0e051
Mick M <mickwpmcdonnell@gmail.com>
2026-08-31 09:53:28 +0930

    Update environment to include complex four rooms and readme with phase 2

--- Code/environment.py
+++ Code/environment.py
@@ -142,3 +142,186 @@ class custom_four_rooms():
         supports.append(support_indices)
 
     return supports
+
+
+class complex_custom_four_rooms():
+  def __init__(self, start_state=(1,1), goal_state=(9,8)):
+    self.doors = DOORS
+    self.actions = ACTIONS
+    self.action_names = ACTION_NAMES
+    self.dimensions = FOUR_ROOMS_DIM
+    self.states = self._build_states()
+    self.n_states = len(self.states)
+    self.n_actions = len(self.actions)
+    self.state_to_index = {state: index for index, state in enumerate(self.states)}
+    self.start_state = start_state
+    self.current_state = start_state
+    self.goal_state = goal_state
+
+    #room dynamics setup:
+    #SW: "breezy" - mmostly reliable but slight chance to move sideways
+    #NW: "spin" - akin to biased windy
+    #SE: "reversed" - Opposite action executed
+    #NE: "Sticky room" - equal chance to take action or stay still. Some slip chance 
+    #Probabilities are:
+    #intended, clockwise, counter-clockwise, remain still, reversed
+    self.room_dynamics = {
+      # Highly reliable.
+      "south-west": (0.90, 0.05, 0.05, 0.00, 0.00),
+
+      # Strong clockwise drift.
+      "north-west": (0.45, 0.45, 0.10, 0.00, 0.00),
+
+      # Action reversal
+      "south-east": (0.00, 0.00, 0.00, 0.00, 1.00),
+
+      # Sticky/sluggish room (we would want to avoid this ideally)
+      "north-east": (0.40, 0.10, 0.10, 0.40, 0.00),
+
+      # doorways are standard.
+      "doorway": (1.00, 0.00, 0.00, 0.00, 0.00),
+    }
+
+
+  def _build_states(self):
+    states = []
+
+    for x in range(1, self.dimensions - 1):
+      for y in range(1, self.dimensions - 1):
+        position = (x, y)
+        is_internal_wall = ( (x == 5 or y == 5) and position not in self.doors)
+
+        if not is_internal_wall:
+          states.append(position)
+
+    return states
+  
+  def _get_room(self, state):
+    x, y = state
+    if x == 5 or y == 5:
+      return "doorway"
+    if x < 5 and y < 5:
+      return "south-west"
+    if x < 5 and y > 5:
+      return "north-west"
+    if x > 5 and y < 5:
+      return "south-east"
+    return "north-east"
+
+  
+  def _get_realised_actions(self, state, action):
+    intended, clockwise, counter_clockwise, still, reversed = (self.room_dynamics[self._get_room(state)])
+
+    outcomes = [
+      (action, intended),
+      ((action + 1) % self.n_actions, clockwise),
+      ((action - 1) % self.n_actions, counter_clockwise),
+      (None, still),
+      ((action - 2) % self.n_actions, reversed),
+    ]
+
+    return [(realised_action, probability) for realised_action, probability in outcomes if probability > 0.0]
+
+  def _move_deterministic(self, state, action):
+    x, y = state
+    dx, dy = self.actions[action]
+
+    next_state = (x + dx, y + dy)
+
+    if next_state in self.state_to_index:
+      return next_state
+
+    return state
+  
+
+  def move(self, state, action):
+    outcomes = self._get_realised_actions(state, action)
+    realised_action = random.choices([outcome[0] for outcome in outcomes],
+                                     weights=[outcome[1] for outcome in outcomes],
+                                     k=1)[0]
+    if realised_action is None:
+      return state
+
+    return self._move_deterministic(state, realised_action)
+
+
+  def step(self, action, state=None):
+    if state is None:
+      state = self.current_state
+
+    next_state = self.move(state, action)
+    reward = 1 if next_state == self.goal_state else 0
+    terminated = next_state == self.goal_state
+    truncated = False
+    #We may want to know where we are so lets include that now
+    info = {"room": self._get_room(state)} 
+
+    self.current_state = next_state
+
+    return (next_state, reward, terminated, truncated, info)
+
+
+#### VERIFICATION ELEMENTS ####
+  def print_room(self, state=(-1,-1)):
+    #print a 2D grid of states
+    for y in reversed(range(FOUR_ROOMS_DIM)):
+      for x in range(FOUR_ROOMS_DIM):
+        if (x,y) in self.states:
+          if (x,y) == state:
+            print('X', end=' ')
+          elif (x,y) == self.goal_state:
+            print('G', end=' ')
+          else:
+            print('s', end=' ')
+        else:
+          if (x,y) in self.doors:
+            print('d', end=' ') #This shouldnt happen - only here to catch if doors are passed NOT as states
+          else:
+            print('.', end=' ')
+      print()
+
+  def get_next_valid_states(self, state):
+    next_states = set()
+    for action in range(self.n_actions):
+      next_state = self._move_deterministic(state, action)
+      if next_state in self.states and next_state != state:
+        next_states.add(next_state)
+    return list(next_states)
+
+
+  def get_transition_probabilities(self, state, action):
+    outcomes = self._get_realised_actions(state, action)
+    
+    probabilities = {}
+
+    for realised_action, probability in outcomes:
+      if realised_action is None:
+        next_state = state
+      else:
+        next_state = self._move_deterministic(state, realised_action)
+
+      probabilities[next_state] = (probabilities.get(next_state, 0.0) + probability)
+
+    return probabilities
+
+  def make_local_supports(self):
+    supports = []
+
+    for state in self.states:
+        possible_states = {state}
+
+        for action in range(self.n_actions):
+            next_state = self._move_deterministic(
+                state,
+                action,
+            )
+            possible_states.add(next_state)
+
+        support_indices = [
+            self.state_to_index[next_state]
+            for next_state in possible_states
+        ]
+
+        supports.append(support_indices)
+
+    return supports
--- README.md
+++ README.md
@@ -5,8 +5,7 @@ This repo contains the work in progress code and artifacts for the thesis in pro
 This readme will evolve with the thesis progress.
 
 ## Current progress:
-- Literature review complete
-- Experimental methodology drafted
+Literature review and Experimental methodology complete. Experimental plan:
   - Prelim phase. Create custom four rooms environment and Bellman inversion.
   - Phase 1. Extract inferred world model and probabalistic trajectories from goal conditioned agents and compare to live environment trajectories.
   - Phase 2. Create multiple distinct goal conditioned agents and contrast agent policy performance on own model vs other agent models. The theory is an agent will perform well on trained goals using its own internal model and more generally, on internal models that are well representative of the environment. It will perform poorly on internal models that are only locally representative of the environment. 
@@ -18,22 +17,26 @@ Custom four rooms environment based on paper's implementation complete including
 Bellman extraction for both deterministic (special case) and stochastic kernals complete
 
 ### Phase 1
-Preliminary work complete:
 - Goal conditioned Q learning implemented
 - World model extraction functional
 - Step wise probability distribution (Monte Carlo) implemented using agent policy and extracted world model.
 - Probability distribution comparison between agent world model simulated trajectories and live agent in environment trajectories.
 - Probability distributions visualised (both world model alone and world model compared to live agent trajectories) with example live trajectory overlaid
+- Probability distribution delta quantified including KL Divergence between the world model estimate and both the live trajectory samples and the real underlying probability distribution. Single live trajectory used as reference distribution allowing us to compare the relative divergence between distributions.
 
-**Next steps:** Quantify probability distribution delta. This will likely include KL Divergence between the world model estimate and both the live trajectory samples and the real underlying probability distribution. This will also likely be compared to the KL Divergence between live trajectories and the underlying true distribution as the reference distribution, and potentially a random sample and second live trajectory run against the original live trajectory as the reference distribution. This will allow us to compare the relative divergence between distributions.
-
-Once we have the structure for the above complete, the robust next step is to run this approach for multiple seeds to ensure we get a broad sample
+### Phase 2
+- Multi model training and comparison complete
+- Visualisations of both model rankings by metric (KL and TV) 
+- Initial goal success metric implemented
+- Complex four rooms implemented where each individual room has a unique environment element.
 
 ## Current results
 
-Phase 1 is progressing well and it is fascinating seeing the probability distributions unfold. Preliminary experimenting shows that we sometimes end up with key differences between inferred trajectories and live trajectories; this is generally mitigated by increased training but can also be indicative of poor goal spread. More generally this is an aspect which we look much further into, particularly in Phase 3
+Phase 1 progressed well and it is fascinating seeing the probability distributions unfold. Preliminary experimenting shows that we sometimes end up with key differences between inferred trajectories and live trajectories; this is generally mitigated by increased training but can also be indicative of poor goal spread. More generally this is an aspect which we look much further into, particularly in Phase 3
 
 This image is an example of the resulting probabiilty distribution from 100000 trajectories in both the inferred model (P hat) and the live agent in the environment.
 
 <img width="1193" height="536" alt="comparison" src="https://github.com/user-attachments/assets/0b1836f4-27e0-4a3f-ab29-794e8a1a8d1a" />
 
+
+The initial Phase 2 work with the windy four rooms environment consisted of multiple agents with the same goals and random starts. This did not provide a significant difference as the model experiences were largely comparable. Implementing the complex custom four rooms allowed the development of 'room specialist' agents which only train on a single room and a generalist that has a goal in each room. This gives a more extreme contrast between models; room specialists still gain knowledge of other room dynamics through exploration however the understanding is much more focussed to their particular room. 
